d9925affc7f5fac8b154c54bb1fe35fcd1810f06
Phyllis Wong <wong.phyllism@gmail.com>
2017-10-08 16:09:25 -0700

    arrays lesson

--- teamTreeHouse.playground/Pages/Swift Basics.xcplaygroundpage/Contents.swift
+++ teamTreeHouse.playground/Pages/Swift Basics.xcplaygroundpage/Contents.swift
@@ -2,7 +2,20 @@
 
 import Foundation
 
-var str = "Hello, playground"
-
 let favProgrammingLanguage = "Swift"
+
+/*
+ Collections and Control Flow
+ Arrays
+*/
+
+// These 2 array notations are equivalent
+var someArr: [String] = []
+var todo = [String]()
+// the append method is great for add a single element
+todo.append("Finish collections course")
+// the =+ operator is great for adding multiple items
+todo += ["buy groceries", "respond to emails"]
+
+print(todo)
 
