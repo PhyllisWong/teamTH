729c40c22daa94e186f6c70efd53d4a6ea0dfd69
Phyllis Wong <wong.phyllism@gmail.com>
2017-10-12 10:08:00 -0700

    functions python

--- Python/basicPython.py
+++ Python/basicPython.py
@@ -2,6 +2,18 @@
 
 print('Hello World!')
 # Different errors:
-# [NameError, unsupported operand type(s), SyntaxError]
+# [NameError, unsupported operand type(s), SyntaxError, EOL = end of line]
 
 """Variables"""
+fav_fruit = 'blueberries'
+fav_num = 14
+
+years = 41
+days = years * 365
+weeks = days / 7
+
+flavors = ['chocolate', 'mint', 'strawberry']
+strFlavors = ', '.join(flavors)
+
+# print("My favorite flavors are " + strFlavors)
+print("My favorite flavors are: {}".format(", ".join(flavors)))
