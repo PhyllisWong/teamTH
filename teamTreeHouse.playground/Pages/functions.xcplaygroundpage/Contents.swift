227d6a379fa2c9318ab7ace9921bcd601aa2bc75
Phyllis Wong <wong.phyllism@gmail.com>
2017-10-13 10:27:25 -0700

    completed functions team treehosue

--- teamTreeHouse.playground/Pages/functions.xcplaygroundpage/Contents.swift
+++ teamTreeHouse.playground/Pages/functions.xcplaygroundpage/Contents.swift
@@ -10,7 +10,7 @@ func area(length: Int, width: Int) -> Int {
 let areaOfFirstRoom = area(length: 10, width: 12)
 let areaOfSecondRoom = area(length: 15, width: 22)
 
-func carpetCostHaving(length: Int, width: Int, carpetColor color: String = "tan") -> (Int, String)) {
+func carpetCostHaving(length: Int, width: Int, carpetColor color: String = "tan") -> (price: Int, carpetColor: String) {
     // grey carpet - $1 p/sq ft
     // tan carpet - $2 p/sq ft
     // deep blue carpet - $4 p/sq ft
@@ -25,7 +25,40 @@ func carpetCostHaving(length: Int, width: Int, carpetColor color: String = "tan"
     default : price = 0
         
     }
-    return price
+    print(price, color)
+    return (price, color)
 }
 
-carpetCostHaving(length: 10, width: 20)
+let result = carpetCostHaving(length: 10, width: 20)
+print(result.price)
+print(result.carpetColor)
+//print("hello world")
+
+func arrModifier(array: [Int]) {
+    var arrOfInts = array
+    arrOfInts.append(5)
+    var secondArrOfInts = arrOfInts
+}
+
+var arrOfInts = [1, 2, 3, 4]
+arrModifier(array: arrOfInts)
+
+func coordinates(for location: String) -> (Double, Double) {
+    var latLon = (0.0, 0.0)
+    
+    switch location {
+    case "Eiffel Tower" : latLon = (48.8582, 2.2945)
+    case "Great Pyramid" : latLon =  (29.9792, 31.1344)
+    case "Sydney Opera House" : latLon =  (33.8587, 151.2140)
+    default : break
+    }
+    return latLon
+}
+
+coordinates(for: "Eiffel Tower")
+
+
+
+
+
+
