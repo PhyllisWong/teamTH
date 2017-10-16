8c5904051c49562fa324781098b7abf9d416c8ca
Phyllis Wong <wong.phyllism@gmail.com>
2017-10-16 15:58:46 -0700

    Switch Statements with OOP

--- teamTreeHouse.playground/Pages/Value vs Reference.xcplaygroundpage/Contents.swift
+++ teamTreeHouse.playground/Pages/Value vs Reference.xcplaygroundpage/Contents.swift
@@ -27,7 +27,27 @@ var anotherPerson = somePerson
 somePerson.email = "t.cook@apple.com"
 print(anotherPerson.email)
 
+// Value Types
 
+struct Tag {
+    let name: String
+}
+
+struct Post {
+    let title: String
+    let author: String
+    let tag: Tag
+    
+    func description() -> String {
+        let description = "\(self.title) by \(self.author). Filed under \(self.tag.name)"
+        return description
+    }
+}
+
+let someTag = Tag(name: "swift")
+let firstPost = Post(title: "iOS Development", author: "Apple", tag: someTag)
+let postDescription = firstPost.description()
+print(postDescription)
 
 
 
