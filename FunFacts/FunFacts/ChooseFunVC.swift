2533a2c594aa3e0f403c43cbfdccf2496587fb05
Phyllis Wong <wong.phyllism@gmail.com>
2017-10-18 09:30:04 -0700

    Nav system fully functioning

--- FunFacts/FunFacts/ChooseFunVC.swift
+++ FunFacts/FunFacts/ChooseFunVC.swift
@@ -14,25 +14,33 @@ class ChooseFunVC: UIViewController {
     @IBAction func didPressFunFacts(_ sender: UIButton) {
         goToFunFactsVC()
     }
-    
     @IBAction func didPressTrivia(_ sender: UIButton) {
+        goToTriviaVC()
     }
-    
-    
     @IBAction func didPressDadJokes(_ sender: UIButton) {
-        
+        goToDadJokesVC()
     }
     func goToFunFactsVC() {
-        
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        
         let funFactsVC = storyboard.instantiateViewController(withIdentifier: "FunFactsVC") as! FunFactsVC
-        
         // TODO: Make sure to set the *delegate* on the FunFactsViewController
         
         self.navigationController?.pushViewController(funFactsVC, animated: true)
     }
-    
+    func goToTriviaVC() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let triviaVC = storyboard.instantiateViewController(withIdentifier: "TriviaVC") as! TriviaVC
+        // TODO: Make sure to set the *delegate* on the FunFactsViewController
+        
+        self.navigationController?.pushViewController(triviaVC, animated: true)
+    }
+    func goToDadJokesVC() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let dadJokesVC = storyboard.instantiateViewController(withIdentifier: "DadJokesVC") as! DadJokesVC
+        // TODO: Make sure to set the *delegate* on the FunFactsViewController
+        
+        self.navigationController?.pushViewController(dadJokesVC, animated: true)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
