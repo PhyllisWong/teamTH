297e1b67823dd810026cf049d74ce3d11e345186
Phyllis Wong <wong.phyllism@gmail.com>
2018-01-01 19:25:35 -0800

    random number generator func

--- Protocols.playground/Pages/protocolsBasics.xcplaygroundpage/Contents.swift
+++ Protocols.playground/Pages/protocolsBasics.xcplaygroundpage/Contents.swift
@@ -87,13 +87,85 @@ let decemberPay = pay(employee: employee)
 print(decemberPay)
 
 
+// Losely related types
+protocol Blendable {
+    func blend()
+}
 
+class Fruit: Blendable {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func blend() {
+        print("I'm mush")
+    }
+}
 
+class Dairy {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+}
 
+// Subclasses of Dairy
+class Cheese: Dairy {}
 
+class Milk: Dairy, Blendable {
+    func blend() {
+        print("I haven't changed at all!")
+    }
+}
 
+func makeSmoothie(with ingredients: [Blendable]) {
+    for ingredient in ingredients {
+        ingredient.blend()
+    }
+}
 
+let strawberry = Fruit(name: "Strawberry")
+let cheddar = Cheese(name: "Cheddar")
+let chocoMilk = Milk(name: "Chocolate")
 
+let ingredients: [Blendable] = [strawberry, chocoMilk]
+
+makeSmoothie(with: ingredients)
+
+// random number generator
+
+protocol RandomNumberGenerator {
+    func random() -> Double
+}
+
+class LinearCongruentGenerator: RandomNumberGenerator {
+    var lastRandom = 42.0
+    let m = 139968.0
+    let a = 3877.0
+    let c = 29573.0
+    
+    func random() -> Double {
+        lastRandom = ((lastRandom * a + c).truncatingRemainder(dividingBy: m))
+        return lastRandom/m
+    }
+}
+
+class Dice {
+    let sides: Int
+    let generator: RandomNumberGenerator
+    
+    init(sides: Int, generator: RandomNumberGenerator) {
+        self.sides = sides
+        self.generator = generator
+    }
+    
+    func roll() -> Int {
+        return Int(generator.random() * Double(sides)) + 1
+    }
+}
 
 
 
