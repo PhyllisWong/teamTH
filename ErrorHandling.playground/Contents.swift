be8c752b4a7f3df029e9242489b22fd2d63435d4
Phyllis Wong <wong.phyllism@gmail.com>
2018-01-04 16:01:28 -0800

    responding to throw errors

--- ErrorHandling.playground/Contents.swift
+++ ErrorHandling.playground/Contents.swift
@@ -6,16 +6,42 @@ struct Friend {
 }
 
 enum FriendError: Error {
-    case invalidData
+    case invalidData(description: String)
+    case someError
 }
 
 func friend(from dict: [String : String]) throws -> Friend {
-    guard let name = dict["name"], let age = dict["age"] else {
+    guard let name = dict["name"] else {
         // If error is thrown, function exits
-        throw FriendError.invalidData
+        throw FriendError.invalidData(description: "Invalid name value")
     }
     
+    guard let age = dict["age"] else {
+        throw FriendError.invalidData(description: "Invalid age value")
+    }
     let address = dict["address"]
-    
     return Friend(name: name, age: age, address: address)
 }
+
+func send(message: String, to friend: Friend) {}
+
+let response = ["name": "Phyllis", "ag": "24", "address": "someAddress"]
+
+do {
+    let myFriend = try friend(from: response)
+    send(message: "Test", to: myFriend)
+} catch FriendError.invalidData(let description) {
+    // Inform the user that they passed in invalidData
+    print(description)
+} catch FriendError.someError {
+    
+}
+
+
+
+
+
+
+
+
+
