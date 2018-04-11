55f2a4f8242ae10b2c620a9f758a5ac804aed62f
Phyllis <wong.phyllism@gmail.com>
2018-04-10 17:21:26 -0700

    testing bintrees module

--- hackerrank_challenges.py
+++ hackerrank_challenges.py
@@ -1,13 +1,42 @@
-collection = ["1", "4", "3", "2"]
+import sys
+from treelib import *
+from treelib.tree import DuplicatedNodeIdError
+from itertools import izip_longest
 
-def reversed_list():
-    for i in range(1, len(collection)+1):
-        print('i: {}, collection[-i]: {}'.format(i, collection[-i]))
+
+def createTree():
+    items = [1, 2, 3, 4, 5, 6, 7]
+    # tree = BinarySearchTree(items)
+    tree = Tree(items)
+    return tree
 
 
-def main():
-    reversed_list()
+def check(self, root, min, max):
+    print('min: {}, max: {}'.format(min, max))
+    if root == None:
+        return True
+    if root.data <= min or root.data >= max:
+        return False
+    return check(root.left, min, root.data) and check(root.right, root.data, max)
+
+
+def check_binary_search_tree_(self, root):
+    return check(root, float('-inf'), float('inf'))
+
+
+def test_binary_search_tree(tree):
+    node = Node(root.data)
+    node.check_binary_search_tree_(root)
+
+
+
+def main(self):
+    tree = createTree()
+    print(tree)
+
+
 
 
 if __name__ == '__main__':
-      main()
+      # main()
+    test_binary_search_tree()
