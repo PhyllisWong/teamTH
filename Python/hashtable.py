6ebc308f70c458713687e341fc0524d95c0fc8c7
Phyllis <wong.phyllism@gmail.com>
2018-05-08 13:45:20 -0700

    Add keys, values, items, and length methods

--- Python/hashtable.py
+++ Python/hashtable.py
@@ -16,19 +16,35 @@ class HashTable(object):
     def keys(self):
         '''Return a list with all the keys in this HashTable'''
         # Collect all keys in each of the buckets
-        pass
+        all_keys = []
+        for bucket in self.buckets:
+            for key, value in bucket.items:
+                all_keys.append(key)
+        return all_keys
 
     def values(self):
         '''Return a list of all the values in this HashTable'''
-        pass
+        all_values = []
+        for bucket in self.buckets:
+            for key, value in bucket.items:
+                all_values.append(value)
+        return all_values
 
     def items(self):
         '''Return a list of all the entries in this HashTable'''
-        pass
+        all_items = []
+        for bucket in self.buckets:
+            # extend appends each item of an iterable
+            all_items.extend(bucket.items())
+        return all_items
 
     def length(self):
         '''Return the number of key-value entries by traversing it's buckets'''
-        pass
+        item_count = 0
+        for bucket in self.buckets:
+            item_count += len(bucket)
+        self.size = item_count
+        return item_count
 
     def contains(self, key):
         '''Return a boolean value whether this hashtable contains a given key'''
@@ -45,5 +61,3 @@ class HashTable(object):
     def delete(self, key):
         '''Delete the given key and its associated value, or raise KeyError'''
         pass
-
-    
